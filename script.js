9e0dbdcc435bb3455ebba22fd803878fdec9c200
debscoding-girl05 <dtakouessa@gmail.com>
2025-02-21 21:41:30 +0100

    Added Script and message

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get references to the button and the message
+const button = document.getElementById("myButton");
+const message = document.getElementById("message");
+
+// Add an event listener for the button click
+button.addEventListener("click", function () {
+  // Show the message when the button is clicked
+  message.style.display = "block";
+});
